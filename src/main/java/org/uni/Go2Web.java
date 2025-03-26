c58eb668ec81d8393733c372959d9ad9a8fc5006
Eduard Smelov <esmelov@griddynamics.com>
2025-03-26 10:20:48 +0200

    Solve the issue with HTTP Request

--- src/main/java/org/uni/Go2Web.java
+++ src/main/java/org/uni/Go2Web.java
@@ -1,3 +1,5 @@
+package org.uni;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -15,6 +17,12 @@ public class Go2Web {
         if (args.length >= 2) {
             String option = args[0];
             StringBuilder valueBuilder = new StringBuilder(args[1]);
+
+            if (option.equals("-s") && args.length > 2) {
+                for (int i = 2; i < args.length; i++) {
+                    valueBuilder.append(" ").append(args[i]);
+                }
+            }
 
             String value = valueBuilder.toString();
 
@@ -39,7 +47,7 @@ public class Go2Web {
     private static void printHelp() {
         System.out.println("Usage:");
         System.out.println("  go2web -u <URL>         # make an HTTP request to the specified URL and print the response");
-        System.out.println("  go2web -s <search-term> # make an HTTP request to search the term using Google and print top 10 results");
+        System.out.println("  go2web -s <search-term> # make an HTTP request to search the term using Bing and print top 10 results");
         System.out.println("  go2web -h               # show this help");
     }
 
@@ -56,12 +64,16 @@ public class Go2Web {
             path += "?" + url.getQuery();
         }
 
+        System.out.println("Connecting to: " + host + " on port " + port);
+        System.out.println("Requesting path: " + path);
+        System.out.println();
+
         try (Socket socket = new Socket(host, port)) {
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             out.print("GET " + path + " HTTP/1.1\r\n");
             out.print("Host: " + host + "\r\n");
             out.print("Connection: close\r\n");
-            out.print("User-Agent: Go2Web/1.0\r\n");
+            out.print("User-Agent: org.uni.Go2Web/1.0\r\n");
             out.print("\r\n");
             out.flush();
 
@@ -73,14 +85,28 @@ public class Go2Web {
             }
 
             String htmlResponse = response.toString();
-            String cleanResponse = removeHtmlTags(htmlResponse);
 
             int bodyStart = htmlResponse.indexOf("\r\n\r\n");
             if (bodyStart != -1) {
-                cleanResponse = removeHtmlTags(htmlResponse.substring(bodyStart + 4));
-            }
+                String headers = htmlResponse.substring(0, bodyStart);
+                String body = htmlResponse.substring(bodyStart + 4);
 
-            System.out.println(cleanResponse);
+                String[] headerLines = headers.split("\r\n");
+                if (headerLines.length > 0) {
+                    System.out.println("Status: " + headerLines[0]);
+                    System.out.println("\nResponse Headers:");
+                    for (int i = 1; i < headerLines.length; i++) {
+                        System.out.println("  " + headerLines[i]);
+                    }
+                }
+
+                String cleanBody = removeHtmlTags(body);
+                System.out.println("\nResponse Body:");
+                System.out.println(cleanBody);
+            } else {
+                String cleanResponse = removeHtmlTags(htmlResponse);
+                System.out.println(cleanResponse);
+            }
         }
     }
 
